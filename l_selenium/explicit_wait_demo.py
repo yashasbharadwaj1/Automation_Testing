5dd0dff2bcef48d9f81cd64add6678ea01f915d6
Yashas <yashasbharadwaj6@gmail.com>
2024-07-09 12:46:57 +0530

    explicit wait

--- l_selenium/explicit_wait_demo.py
+++ l_selenium/explicit_wait_demo.py
@@ -2,11 +2,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions 
+
 driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 
 # implicit wait -- works as a global timeout
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 
 driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
 
@@ -35,6 +38,9 @@ driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
 
+# Explicit wait for specific element
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,".promoInfo")))
 
 code_applied_msg = driver.find_element(By.CLASS_NAME, "promoInfo").text
 
